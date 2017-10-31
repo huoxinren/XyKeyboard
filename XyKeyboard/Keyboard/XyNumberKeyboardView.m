cb079d4015436894d51fdd54a428887a92bdb434
Xy <xy@XydeMacBook-Pro.local>
2017-10-31 14:55:09 +0800

    测试用

--- XyKeyboard/Keyboard/XyNumberKeyboardView.m
+++ XyKeyboard/Keyboard/XyNumberKeyboardView.m
@@ -15,7 +15,7 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+    // Drawing code ceshi
 }
 */
 
